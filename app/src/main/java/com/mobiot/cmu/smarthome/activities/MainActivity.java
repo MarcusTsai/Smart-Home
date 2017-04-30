f60ba7f867e25ef507b26cd841f595783444fb85
MarcusTsai <mingchia.ts@gmail.com>
2017-04-29 20:02:50 -0700

    add temperature, humidity, and QR code

--- app/src/main/java/com/mobiot/cmu/smarthome/activities/MainActivity.java
+++ app/src/main/java/com/mobiot/cmu/smarthome/activities/MainActivity.java
@@ -1,15 +1,19 @@
 package com.mobiot.cmu.smarthome.activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Environment;
 import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Base64;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -18,21 +22,36 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.mobiot.cmu.smarthome.R;
 
+import org.json.JSONObject;
+
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.Locale;
+
+import static android.R.string.no;
+import static android.R.string.yes;
 
 public class MainActivity extends AppCompatActivity {
     private TextToSpeech t1;
     private int count = 1;
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private String instanceID = null;
+    private DatabaseReference myRef = null;
+    private TextView temperatureText;
+    private TextView humidityText;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("deviceTest").child("1").child("current");
-        Button facedetection = (Button) findViewById(R.id.facedetection);
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+
+        temperatureText = (TextView) findViewById(R.id.tempText);
+        humidityText = (TextView) findViewById(R.id.humidText);
+        Button registerButton = (Button) findViewById(R.id.register);
         t1 = new TextToSpeech(MainActivity.this, new TextToSpeech.OnInitListener() {
             @Override
             public void onInit(int status) {
@@ -42,7 +61,53 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        Bundle bundle = getIntent().getExtras();
+
+        if(bundle != null) {
+           instanceID = bundle.get("instanceID").toString();
+           System.out.println("instanceID:" + instanceID);
+           setReference();
+        }
+
+        if(instanceID == null) {
+            AlertDialog diag = new AlertDialog.Builder(this)
+                .setTitle("Initial Set Up")
+                .setMessage("Please Register at First")
+                .setPositiveButton(yes, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent(MainActivity.this, QRActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton(no, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                })
+                .setIcon(R.drawable.common_google_signin_btn_icon_dark_normal).show();
+
+        } else {
+            myRef = database.getReference(instanceID).child("1").child("current");
+            setReference();
+
+        }
+
+        registerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, QRActivity.class);
+                startActivity(intent);
+            }
+
+        });
+
+    }
+
+    private void setReference () {
         // Read from the database
+        DatabaseReference myRef = database.getReference(instanceID).child("1").child("current");
         myRef.child("clientAudio").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -71,6 +136,7 @@ public class MainActivity extends AppCompatActivity {
                 // Failed to read value
             }
         });
+
         myRef.child("message").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
@@ -79,6 +145,7 @@ public class MainActivity extends AppCompatActivity {
 
 
                 String value = dataSnapshot.getValue(String.class);
+                System.out.println("datasnapshot:" + dataSnapshot);
                 t1.speak(value, TextToSpeech.QUEUE_FLUSH, null, "" + (count++));
 
             }
@@ -89,16 +156,40 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        facedetection.setOnClickListener(new View.OnClickListener(){
+
+        myRef.child("environment").addValueEventListener(new ValueEventListener() {
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, MultiTrackerActivity.class);
-                startActivity(intent);
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                try {
+                    JSONObject config = new JSONObject((HashMap) dataSnapshot.getValue());
+                    System.out.println("config:" + config);
+                    String temp = config.getString("temperature");
+                    temperatureText.setText(temp + "'C");
+
+                    String humid = config.getString("humidity");
+                    humidityText.setText(humid + "%");
+
+                    Boolean motiondetect = config.getBoolean("motion");
+                    if(motiondetect.equals(true)) {
+                        Intent intent = new Intent(MainActivity.this, MultiTrackerActivity.class);
+                        startActivity(intent);
+
+                    }
+                } catch (Exception e) {
+                    Toast.makeText(MainActivity.this, "Json Download Error", Toast.LENGTH_LONG).show();
+                    System.out.println("env:" + dataSnapshot);
+                    e.printStackTrace();
+                }
             }
 
-        });
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
 
+            }
+        });
     }
+
     private void playAudio(File file) {
         MediaPlayer player = new MediaPlayer();
         String path = file.getAbsolutePath();
---
+++ app/src/main/java/com/mobiot/cmu/smarthome/activities/QRActivity.java
@@ -0,0 +1,74 @@
+package com.mobiot.cmu.smarthome.activities;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
+import com.mobiot.cmu.smarthome.R;
+
+public class QRActivity extends AppCompatActivity {
+    private static String androidID = null;
+    private Button scanButton;
+    private TextView instTextView;
+    private String instanceID = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr);
+
+        instTextView = (TextView) findViewById(R.id.instIDTextView);
+        scanButton = (Button) findViewById(R.id.scanButton);
+
+        scanButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Propagate Main Activity to IntentIntegrator
+                IntentIntegrator intentIntegrator = new IntentIntegrator(QRActivity.this);
+                intentIntegrator.setDesiredBarcodeFormats(IntentIntegrator.ALL_CODE_TYPES);
+                intentIntegrator.setPrompt("Scan");
+                intentIntegrator.setCameraId(0);
+                intentIntegrator.setBeepEnabled(false);
+                intentIntegrator.setBarcodeImageEnabled(false);
+                intentIntegrator.initiateScan();
+            }
+        });
+
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        IntentResult result = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
+
+        if (result != null) {
+            if (result.getContents() == null) {
+                Log.d("Main Activity Scan", "Cancel Scan ====================================");
+                Toast.makeText(this, "Canceled", Toast.LENGTH_LONG).show();
+            } else {
+                // get result from Zxing's lib
+                Log.d("Main Activity Scan", "Scanned ====================================");
+                // start google search activity immediately
+                System.out.println("result: " + result.getContents());
+
+                instanceID = result.getContents();
+                System.out.println("URL: " + instanceID);
+                instTextView.setText(instanceID);
+                if(!instanceID.isEmpty()) {
+                    Intent intentNext = new Intent(QRActivity.this, MainActivity.class);
+                    intentNext.putExtra("instanceID", instanceID);
+                    startActivity(intentNext);
+                }
+            }
+        } else {
+            /**
+             * Repeatedly call
+             */
+            super.onActivityResult(requestCode, resultCode, intent);
+        }
+    }
+}
